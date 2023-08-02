38338f459c9e55bc8e2ab923663b36d9c891f362
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-08-02 22:38:58 +0530

    new things added

---
+++ src/array_traversal.java
@@ -0,0 +1,10 @@
+// array traversal
+public class array_traversal {
+    public static void main(String[] args) {
+        int[] arr = {20 , 30 ,90,40,80,60};
+        for (int i = 0 ; i<= arr.length ;i++){
+            System.out.print(arr[i] +" ");
+        }
+
+    }
+}
