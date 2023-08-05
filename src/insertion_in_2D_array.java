f0d8248b1dcfe6f569977fe717287eb59b63e76e
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-08-05 17:21:09 +0530

    2D array Added updated

---
+++ src/insertion_in_2D_array.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class insertion_in_2D_array {
+    int arr [][] = null;
+
+    public  insertion_in_2D_array (int numberofRows , int numberOfColoumns){
+        this.arr = new int[numberofRows][numberOfColoumns];
+        for (int row = 0 ;row< arr.length ; row++){
+            for (int col = 0 ; col<arr[0].length ; col++){
+                arr[row][col]= Integer.MIN_VALUE;
+            }
+        }
+    }
+
+    public void insertValueInTheArray(int row ,int col , int value){
+        try {
+            if (arr[row][col] == Integer.MIN_VALUE){
+                arr[row][col] = value;
+                System.out.println("The value in inserted sucessfully ");
+            }
+            else {
+                System.out.println("This cell is already occupied");
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid index for 2D array");
+        }
+    }
+
+    public static void main(String[] args) {
+        insertion_in_2D_array obj = new insertion_in_2D_array(3,3);
+        obj.insertValueInTheArray(1,2,30);
+        obj.insertValueInTheArray(2,2,50);
+        System.out.println(Arrays.deepToString(obj.arr));
+
+    }
+}
