2d9ca0f372d112e62e28c75361aef55ef947f400
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-08-09 16:31:32 +0530

    updated searching_element_2D_array

--- src/searching_element_2D_array.java
+++ src/searching_element_2D_array.java
@@ -6,21 +6,22 @@ public class searching_element_2D_array {
         System.out.println("Enter the value you want to find");
         int value = sc.nextInt();
         int [][] dym = {{10 ,20,60} ,{80 ,90 ,100},{70 ,40 ,45}};
-        try {
+
             for (int row = 0 ; row<dym.length ;row++){
                 for (int col = 0 ; col<dym[0].length ; col++){
                     if(dym[row][col]== value){
-                        System.out.println("Value is found row : " +row +"col : "+ col);
-
+                        System.out.println("Value is found row:" +row + " col:"+ col);
+                        return;
                     }
 
                 }
             }
-        }catch (ArrayIndexOutOfBoundsException exception){
-            System.out.println("Element not found");
+        System.out.println("Value is not found");
+        }
+
         }
 
 
 
-    }
-}
+
+
