4c6f6c44b81d0fadb445177f406c101b14983424
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-08-12 11:09:21 +0530

    updated delete_element_in_2D_array

---
+++ src/array_project.java
@@ -0,0 +1,20 @@
+import java .util.Scanner;
+public class array_project {
+    public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+        System.out.print("How many days temperature :");
+        int numDays = sc.nextInt();
+        int sum= 0;
+        for (int i =1 ;i<=numDays ;i++){
+            System.out.print("Day " + i+ "'s high temp: ");
+            int next = sc.nextInt();
+            sum = sum+next;
+
+        }
+        double average = sum/numDays;
+        System.out.println();
+        System.out.println("The average temperature is :" + average);
+
+        }
+    }
+}
